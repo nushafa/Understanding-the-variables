c9cbede6f4ea0ac9a2a3b993ae42f6f1e33e87b0
Abiat Nushafa <nushafa1@gmail.com>
2025-05-20 20:56:31 +0600

    Add files via upload

---
+++ Understanding the variables.py
@@ -0,0 +1,13 @@
+#creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+#User input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
